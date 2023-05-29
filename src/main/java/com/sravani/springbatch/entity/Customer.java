600f01d48b6f4d3cfe3793ea549fa9ff0f61d3c3
sravani-gloify <akula.sravani@gloify.com>
2023-05-29 11:34:10 +0530

    second commit

---
+++ src/main/java/com/sravani/springbatch/entity/Customer.java
@@ -0,0 +1,33 @@
+package com.sravani.springbatch.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="CUSTOMERS_INFO")
+@Data
+public class Customer {
+	 @Id
+	    @Column(name = "CUSTOMER_ID")
+	    private int id;
+	    @Column(name = "FIRST_NAME")
+	    private String firstName;
+	    @Column(name = "LAST_NAME")
+	    private String lastName;
+	    @Column(name = "EMAIL")
+	    private String email;
+	    @Column(name = "GENDER")
+	    private String gender;
+	    @Column(name = "CONTACT")
+	    private String contactNo;
+	    @Column(name = "COUNTRY")
+	    private String country;
+	    @Column(name = "DOB")
+	    private String dob;
+	
+ 
+}
